c0a57e1a4022364cf6f0658acadb42931df3e2e9
KazuyaSasaki <kazuya.sasaki39@gmail.com>
2020-04-27 01:25:18 +0900

    追加 Validation 入力フォームチェック

--- src/main/java/com/sample/controller/PostController.java
+++ src/main/java/com/sample/controller/PostController.java
@@ -3,6 +3,8 @@ package com.sample.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -34,9 +36,19 @@ public class PostController {
     public String post_create(
     		Model model,
     		@ModelAttribute("postForm")
-    		PostForm postForm
+    		@Validated
+    		PostForm postForm,
+    		BindingResult result
     	) {
 
+    	if(result.hasErrors()) {
+    		
+        	model.addAttribute("message", "This is post page"); 	
+        	model.addAttribute("postForm", postForm); 	
+        	
+            return "post_form";
+    	}
+    	
     	postService.createArticle(postForm);
     	
         return "redirect:/";
